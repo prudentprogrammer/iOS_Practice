dc8b8bf47f5d89bdb3a58207b3c14d795ef58bee
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-03-05 13:39:07 -0800

    Added description function to BNRItem class

--- main.m
+++ main.m
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRItem.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
@@ -22,6 +23,12 @@ int main(int argc, const char * argv[]) {
         for(NSString *item in items) {
             NSLog(@"%@", item);
         }
+        
+        BNRItem *item = [[BNRItem alloc] init];
+        item.itemName = @"Red Sofa";
+        item.serialNumber = @"A1B2C";
+        item.valueInDollars = 100;
+        NSLog(@"%@", item);
         
         items = nil;
     }
