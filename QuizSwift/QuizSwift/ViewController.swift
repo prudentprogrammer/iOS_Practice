171b09396fae0bc341ea5c44f62de2260722ebba
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-05-16 21:00:57 -0700

    Added autolayout and models

--- QuizSwift/QuizSwift/ViewController.swift
+++ QuizSwift/QuizSwift/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    var currentQuestionIndex: Int = 0
+    
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Karnataka?",
+        "What is cognac made of?",
+        "Who will win this years IPL?"
+    ]
+    
+    let answers: [String] = [
+        "It is 14",
+        "Bangalore",
+        "Monteplier",
+        "RCB"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        // Increment the counter
+        currentQuestionIndex = (currentQuestionIndex + 1) % questions.count
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
     }
-
-
+    
+    @IBAction func showNextAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
 }
 
