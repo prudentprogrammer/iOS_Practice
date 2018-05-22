c5703f7339e39c91325c46bb54687e9a2248e1cd
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-05-21 23:04:07 -0700

    Added views chapter code

---
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Arjun Bharadwaj on 5/21/18.
+//  Copyright © 2018 Vaadiraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+//    override func viewDidLoad() {
+//        super.viewDidLoad() // Call superview's viewdidload to load superview
+//        
+//        let firstFrame = CGRect(x: 160, y:240, width:100, height: 150)
+//        let firstView = UIView(frame: firstFrame)
+//        firstView.backgroundColor = UIColor.blue
+//        view.addSubview(firstView)
+//        
+//        let secondFrame = CGRect(x: 20, y:30, width:50, height:50)
+//        let secondView = UIView(frame: secondFrame)
+//        secondView.backgroundColor = UIColor.green
+//        view.addSubview(secondView)
+//    }
+}
+
