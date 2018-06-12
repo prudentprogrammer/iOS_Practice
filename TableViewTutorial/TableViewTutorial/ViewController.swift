ca1d1a19c92f932614342dd2ae787c825a1fb992
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-06-11 18:35:45 -0700

    Added data source.

--- TableViewTutorial/TableViewTutorial/ViewController.swift
+++ TableViewTutorial/TableViewTutorial/ViewController.swift
@@ -8,17 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    var myData = [String]() // The data used in tableview
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 0...100 {
+            myData.append("Row #\(i)")
+        }
+        
+        //print(myData)
+        
+        tableView.dataSource = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = UITableViewCell()
+        myCell.textLabel?.text = myData[indexPath.row]
+        return myCell
     }
 
 
